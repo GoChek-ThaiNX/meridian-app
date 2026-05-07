fdefa9e8a53a4e6b08287131848388896eb3cd8a
ThaiNX1 <nxthai22@gmail.com>
2026-05-07 17:04:30 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './GoChek_CRM_V41.jsx'
+import App from './GoChek_CRM_V42.jsx'
 import './index.css'
 import { initDevToolsGuard } from './devtools-guard.js'
 
