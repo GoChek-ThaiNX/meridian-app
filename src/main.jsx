2c394582a60c6ec5d3fd2a77c86be94832002da5
ThaiNX1 <nxthai22@gmail.com>
2026-06-02 08:43:40 +0700

    update ver 46

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './GoChek_CRM_V45.jsx'
+import App from './GoChek_CRM_V46.jsx'
 import './index.css'
 import { initDevToolsGuard } from './devtools-guard.js'
 
